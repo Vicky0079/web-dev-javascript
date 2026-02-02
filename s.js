c097d665381396af60d862cecd3bae724eb22aa3
Vicky0079 <vickylucky00079@gmail.com>
2026-02-02 15:45:15 +0530

    initial

---
+++ s.js
@@ -0,0 +1,298 @@
+// console.log('hello world')
+
+// var x = 10
+// console.log(typeof(x))
+// console.log(x)
+
+// let y = 20;
+// console.log(y)
+
+// const z = 30;
+// console.log(z)
+
+// redeclare
+// var x = 10;
+// var x = 30;   
+// console.log(x)   
+
+// reassign
+// var x = 10;
+// x=40
+// console.log(x)
+
+// let x = 10;
+// x = 30;
+// console.log(x)
+
+// // const x = 30;
+// x=50;
+// var x = 30;
+// console.log(x)
+
+
+// let x = 10;
+// let y = '10.0'
+// console.log(x == y)
+
+//for check vaule and data type
+// let x = 10;
+// let y = '10.0'
+// console.log(x === y)
+
+// for check not equal to vaule
+// let x = 10;
+// let y = 20;
+// console.log(x != y)
+
+// for check datatype and vaule
+// let x = 10;
+// let y = 20;
+// console.log(x !== y)
+
+
+// let x =10;
+//  let y =20;
+
+//  if(x<y){
+//     console.log('first')
+
+//  }else{
+//     console.log('second')
+//  }
+
+// for(let i = 1; i<=5 ; i++){
+//    console.log(i)
+// }
+
+
+
+// for(let i = 1; i<=5 ; i++){
+//    console.log('a');
+// }
+
+// var a = 5; for differ line
+// var b = --a;
+// console.log(b);
+
+
+// var a = 5; for same line
+// var b = a--;
+// console.log(b);
+
+// function
+
+
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+
+// function div(){
+
+
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// console.log("HEllo world")
+// }
+// div()
+
+// function div(){
+//    console.log("This is div function")
+// }
+// div()
+
+
+// let x = function() {
+//    console.log("this is a function")
+// }
+
+// console.log(x)
+
+// let x = () => {
+//    var a = 4
+//    console.log("this is arrow function")
+//    console.log("this is another line")
+// }
+// x()
+
+
+// function paneer(){
+//    console.log('paneer lane jana he')
+// }
+// function mammi(callback){
+//    callback()
+//    console.log('paneer ki sabji banana he')
+// }
+// mammi(paneer)
+
+// function fun(x){
+//    console.log(x)
+// }
+
+// fun(20)
+
+// function paneer(){
+//    console.log('paneer lane jana he')
+// }
+// function mammi(callback){
+//    // console.log(callback)
+//    callback
+//    console.log('paneer ki sabji banana he (sab ji ban rahi he)')
+
+// }
+// mammi(paneer)   
+
+// function parent(){
+
+//    function child(){
+//       let x =20;
+//       console.log(x)
+//    }
+//    child();
+// }
+// parent()
+
+// function sum(){
+//    let x = 20
+//    return x;
+// }
+// let newValue = sum()
+// console.log(newValue)
+
+// function parent() {
+//    let x = 10;
+//    function child() {
+//       console.log(x)
+//    }
+//    return child;
+//    console.log(x)
+// }
+// let newFun = parent()
+// console.log(newFun)
+
+// let x = [1,2,3,4,5,6, 'pratik'];
+// console.log(x)
+  
+
+// let array = [1,2,3,4,'partik',3.4]
+// console.log(array)
+
+// let array = [1,2,3,4,'partik',3.4]
+// console.log(array[4])
+
+// let array = [1,2,3,4,5];
+// console.log(array)
+// function filterElement(element){
+//     retrun (element<2)}
+//     let newArray = array.filter(filterElement)
+//     console.log(newArray)
+
+// let array = [1,2,3,4,5]
+// function total(acc,crr){
+//         return acc+crr
+// }
+// let x = array.reduce(total,0)
+// console.log(x)
+
+
+// let person1 = {
+   
+//     name:'modi',
+//     age:24,
+//     gender:'Gay'
+// }
+
+// let person2 = {
+   
+//     name:'Meloni',
+//     age:24,
+//     gender:'female'
+// }
+
+// let person3 = {
+   
+//     name:'trump',
+//     age:24,
+//     gender:'male'
+// }
+
+// console.log(person1.name)
+// console.log(person2.name)
+// console.log(person3.name)
+// console.log(person1.age)
+// console.log(person2.gender)
+// console.log(person3.age)
+
+
+// personalbar.name = 'yogi'
+// personalbar.gender = 'male'
+
+// delete person1.age
+
+
+
+// let x = 10;
+// let y = 30;
+// let obj = {
+//     x,
+//     y
+// }
+// console.log(obj)
+
+
+// let obj ={
+//     x:10,
+//     y:30
+// }
+// let{x,y}= obj
+// console.log(x,y)
+
+// let obj2 = obj1
+
+// obj2.x = 1000
+
+// console.log(obj1)
+
+const heading = document.getElementById('myHeading');
+console.log(heading)
+
+heading.style.color ="tomato";
+heading.style.backgroundColor ="yellow";
+
+const para = document.getElementsByClassName('para');
+console.log(para)
+
+for(let i=0; i<para.length; i++){
+    para[i].style.backgroundColor = "cyan";
+}
+
+
