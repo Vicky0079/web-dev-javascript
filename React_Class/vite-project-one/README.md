195033a0b73336b4668bd1b073c28e996956f46c
Vicky0079 <vickylucky00079@gmail.com>
2026-03-31 19:51:17 +0530

    react_class

---
+++ React_Class/vite-project-one/README.md
@@ -0,0 +1,16 @@
+# note
+
+1. First letter of the component 
+must be capital
+2. Diffing algo and reconciliation
+3. Only one component (function) 
+allowed to be default export.
+4. Wrap named name export component 
+in { }
+
+'''javascript
+const Demo=()=>{
+    return "hello World"
+}
+export{Demo}
+'''
---
+++ React_Class/vite-project-one/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./App.css"
+import { add, subtract } from './Math.jsx';
+
+
+const App = () => {
+  console.log(add(2,3)); //output:5
+  console.log(subtract(5, 2)); //output:3
+
+  return (
+   <>
+
+    <h1>This is App component</h1>
+  
+   
+   </>
+  )   
+}
+export default App
